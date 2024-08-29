7c022a4089e6b7a35f3521ebae54fd31206ecdb1
HolyBlackCat <holyblckcat@gmail.com>
2024-08-29 12:44:00 +0500

    `decltype((...))` in `M_RETURNS`.

--- include/minimacros/returns.h
+++ include/minimacros/returns.h
@@ -1,7 +1,10 @@
 #pragma once
 
+// Use this as a body of lambda expressions: (or of functions with trailing return type)
+//   [](auto x) M_RETURNS(x.foo())
+// Double parentheses in `decltype(...)` won't usually matter, but it's a good idea to have them e.g. in case you return a global variable.
 #define M_RETURNS(...) \
-    noexcept(noexcept(__VA_ARGS__)) -> decltype(__VA_ARGS__) { return __VA_ARGS__; }
+    noexcept(noexcept(__VA_ARGS__)) -> decltype((__VA_ARGS__)) {return __VA_ARGS__;}
 
 #if M_SHORT_MACROS || M_SHORT_MACROS_RETURNS
 #define RETURNS M_RETURNS
