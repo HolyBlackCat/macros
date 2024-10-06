757b10c8b6fd367a213aac5d94792f970f98ab8c
HolyBlackCat <holyblckcat@gmail.com>
2024-10-06 11:56:31 +0500

    Leading and trailing commas.

---
+++ include/em/macros/meta/comma.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "em/macros/meta/check_emptiness.h"
+
+// Manipulate leading and trailing commas.
+
+// Add a leading/trailing comma if the argument is not empty.
+#define EM_ADD_LEADING_COMMA(...) __VA_OPT__(,) __VA_ARGS__
+#define EM_ADD_TRAILING_COMMA(...) __VA_ARGS__ __VA_OPT__(,)
+
+// Given `, ...`, removes the leading `,`.
+// If the argument is empty, returns nothing.
+// Otherwise, if the argument doesn't start with a `,`, fails.
+#define EM_REMOVE_LEADING_COMMA(x, ...) EM_EXPECT_EMPTY(x) __VA_ARGS__
