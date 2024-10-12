86f297acca7e85e1457a3c78ca9eee9d8d5e47a7
HolyBlackCat <holyblckcat@gmail.com>
2024-10-12 12:44:40 +0500

    Wrap `if consteval`.

---
+++ include/em/macros/portable/if_consteval.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#if __cpp_if_consteval
+#  define EM_IF_CONSTEVAL if consteval
+#  define EM_IF_NOT_CONSTEVAL if !consteval
+#else
+#  define EM_IF_CONSTEVAL if (std::is_constant_evaluated())
+#  define EM_IF_NOT_CONSTEVAL if (!std::is_constant_evaluated())
+#endif
