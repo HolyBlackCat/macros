9c778d72283e353fe591fa3b2b7cd5689617d6d2
HolyBlackCat <holyblckcat@gmail.com>
2024-10-12 12:54:56 +0500

    Detect failing assumptions at compile-time.

--- include/em/macros/portable/assume.h
+++ include/em/macros/portable/assume.h
@@ -1,14 +1,17 @@
 #pragma once
 
 // `EM_ASSUME(cond);` informs the compiler that the condition is true.
+// If the condition is false during constexpr evaluation, causes a compilation error.
 
 #ifndef EM_ASSUME
 #  if __has_cpp_attribute(assume)
 #    define EM_ASSUME(...) [[assume(__VA_ARGS__)]]
 #  elif defined(__clang__)
-#    define EM_ASSUME(...) __builtin_assume(__VA_ARGS__)
+#    include "em/macros/portable/if_consteval.h"
+#    define EM_ASSUME(...) EM_IF_CONSTEVAL {if (__VA_ARGS__) {} else __builtin_unreachable();} else __builtin_assume(__VA_ARGS__)
 #  elif defined(_MSC_VER)
-#    define EM_ASSUME(...) __assume(__VA_ARGS__)
+#    include "em/macros/portable/if_consteval.h"
+#    define EM_ASSUME(...) EM_IF_CONSTEVAL {if (__VA_ARGS__) {} else throw "Assumption is false.";} __assume(__VA_ARGS__)
 #  else
 #    define EM_ASSUME(...) if (__VA_ARGS__) {} else __builtin_unreachable();
 #  endif
--- include/em/macros/portable/if_consteval.h
+++ include/em/macros/portable/if_consteval.h
@@ -4,6 +4,7 @@
 #  define EM_IF_CONSTEVAL if consteval
 #  define EM_IF_NOT_CONSTEVAL if !consteval
 #else
+#  include <type_traits>
 #  define EM_IF_CONSTEVAL if (std::is_constant_evaluated())
 #  define EM_IF_NOT_CONSTEVAL if (!std::is_constant_evaluated())
 #endif
