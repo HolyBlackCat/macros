a72c10f93fcac9ad9eb89c0e6bb9af98322170ae
HolyBlackCat <holyblckcat@gmail.com>
2024-10-18 11:49:23 +0500

    Rename some macro parameters.

--- include/em/macros/portable/canonical_typedefs.h
+++ include/em/macros/portable/canonical_typedefs.h
@@ -16,10 +16,10 @@
 // On Clang, this causes those typedefs to be used in the error message when possible, making them less verbose.
 // This can also affect how type name strings are determined by various libraries that use `__PRETTY_FUNCTION__` internally.
 
-#define EM_CANONICAL_TYPEDEFS(type_, name_, aliases_) \
-    EM_IDENTITY type_ name_; \
+#define EM_CANONICAL_TYPEDEFS(kind_, name_, aliases_) \
+    EM_IDENTITY kind_ name_; \
     EM_END(DETAIL_EM_CANONICAL_TYPEDEFS_LOOP_USING_A aliases_) \
-    DETAIL_EM_CANONICAL_TYPEDEFS(type_, name_, aliases_)
+    DETAIL_EM_CANONICAL_TYPEDEFS(kind_, name_, aliases_)
 
 #define DETAIL_EM_CANONICAL_TYPEDEFS_LOOP_USING_BODY(name_, ...) using name_ = __VA_ARGS__;
 // Adding `EM_IDENTITY()` here to keep the legacy MSVC preprocessor happy.
@@ -30,8 +30,8 @@
 
 #if defined(__has_attribute) && (!defined(EM_ENABLE_CANONICAL_TYPEDEFS) || EM_ENABLE_CANONICAL_TYPEDEFS)
 #if __has_attribute(__preferred_name__)
-#define DETAIL_EM_CANONICAL_TYPEDEFS(type_, name_, aliases_) \
-    EM_IDENTITY type_ \
+#define DETAIL_EM_CANONICAL_TYPEDEFS(kind_, name_, aliases_) \
+    EM_IDENTITY kind_ \
         EM_END(DETAIL_EM_CANONICAL_TYPEDEFS_LOOP_ATTR_A aliases_) \
         name_; \
     DETAIL_EM_CANONICAL_TYPEDEFS_CLANG_WORKAROUND(aliases_)
@@ -64,9 +64,9 @@ namespace em::detail::Macros::CanonicalTypedefs
 #endif
 
 #else // this attribute is not supported
-#define DETAIL_EM_CANONICAL_TYPEDEFS(type_, name_, aliases_)
+#define DETAIL_EM_CANONICAL_TYPEDEFS(kind_, name_, aliases_)
 #endif
 
 #else // no __has_attribute
-#define DETAIL_EM_CANONICAL_TYPEDEFS(type_, name_, aliases_)
+#define DETAIL_EM_CANONICAL_TYPEDEFS(kind_, name_, aliases_)
 #endif
