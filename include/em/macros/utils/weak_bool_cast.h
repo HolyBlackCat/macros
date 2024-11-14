e38465d24a407f722b4e37145f8e304321edad9f
HolyBlackCat <holyblckcat@gmail.com>
2024-11-14 12:24:17 +0500

    Remove `EM_WEAK_BOOL_CAST`.

--- include/em/macros/utils/weak_bool_cast.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-// This is a weak cast to bool that rejects some things that the regular `bool(...)` cast would accept (such as enum-class to bool).
-#define EM_WEAK_BOOL_CAST(...) ((__VA_ARGS__) ? true : false)
