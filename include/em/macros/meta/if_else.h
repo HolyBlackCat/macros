d64b59fcbf673b8270df73a583283df5e470822d
HolyBlackCat <holyblckcat@gmail.com>
2024-11-30 14:19:49 +0300

    Better if/else macros.

--- include/em/macros/meta/if_else.h
+++ include/em/macros/meta/if_else.h
@@ -2,21 +2,17 @@
 
 #include "em/macros/meta/common.h"
 
-// If `...` is empty, returns `fallback` (with parentheses stripped). Otherwise returns `...` unchanged.
-// `fallback` must be parenthesized.
-#define EM_FALLBACK_IF_EMPTY(fallback, ...) EM_CAT(DETAIL_EM_FALLBACK_IF_EMPTY_,__VA_OPT__(1))(fallback, __VA_ARGS__)
-#define DETAIL_EM_FALLBACK_IF_EMPTY_(fallback, ...) EM_IDENTITY fallback
-#define DETAIL_EM_FALLBACK_IF_EMPTY_1(fallback, ...) __VA_ARGS__
+// `EM_IF_NONEMPTY(cond)(a)(b)` expands to `a` if `cond` isn't empty, or to `b` if it is empty. All parts can contain commas.
+#define EM_IF_NONEMPTY(...) EM_CAT(DETAIL_EM_IF_NONEMPTY_, __VA_OPT__(1))
+#define DETAIL_EM_IF_NONEMPTY_(...) DETAIL_EM_IF_ELSE_1
+#define DETAIL_EM_IF_NONEMPTY_1(...) __VA_ARGS__ DETAIL_EM_IF_ELSE_
+#define DETAIL_EM_IF_ELSE_(...)
+#define DETAIL_EM_IF_ELSE_1(...) __VA_ARGS__
 
-// `EM_IF_EMPTY((cond),(a),(b))` expands to `a` if `cond` is empty, or to `b` if it's not empty.
-#define EM_IF_EMPTY(cond, a, b) DETAIL_EM_IF_EMPTY(a, b, EM_IDENTITY cond)
-#define DETAIL_EM_IF_EMPTY(a, b, .../*cond*/) EM_CAT(DETAIL_EM_IF_EMPTY_, __VA_OPT__(1))(a, b)
-#define DETAIL_EM_IF_EMPTY_(a, b) EM_IDENTITY a
-#define DETAIL_EM_IF_EMPTY_1(a, b) EM_IDENTITY b
+// If `...` isn't empty, returns it unchanged. Otherwise returns `fallback`.
+// `fallback` can contain delayed commas.
+#define EM_FALLBACK_IF_EMPTY(fallback, ...) EM_IF_NONEMPTY(__VA_ARGS__)(__VA_ARGS__)(fallback)
 
-// `EM_IF_COMMA((cond),(a),(b))` expands to `a` if `cond` contains at least one comma, or to `b` otherwise.
-#define EM_IF_COMMA(cond, a, b) DETAIL_EM_IF_COMMA(EM_IDENTITY cond x)(a, b)
-#define DETAIL_EM_IF_COMMA(...) DETAIL_EM_IF_COMMA_A(__VA_ARGS__)
-#define DETAIL_EM_IF_COMMA_A(unused, ...) EM_CAT(DETAIL_EM_IF_COMMA_, __VA_OPT__(1))
-#define DETAIL_EM_IF_COMMA_(a, b) EM_IDENTITY b
-#define DETAIL_EM_IF_COMMA_1(a, b) EM_IDENTITY a
+// `EM_IF_COMMA(cond)(a)(b)` expands to `a` if `cond` contains at least one comma, or to `b` otherwise. `a` and `b` are allowed to contain commas.
+#define EM_IF_COMMA(...) DETAIL_EM_IF_COMMA(__VA_ARGS__ x)
+#define DETAIL_EM_IF_COMMA(unused, ...) EM_CAT(DETAIL_EM_IF_NONEMPTY_, __VA_OPT__(1))
