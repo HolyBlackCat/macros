03dcdde864847a91f4d5acec7e4b82a4afd7bf37
HolyBlackCat <holyblckcat@gmail.com>
2024-12-01 19:32:10 +0300

    More types of if/else macros.

--- include/em/macros/meta/if_else.h
+++ include/em/macros/meta/if_else.h
@@ -2,17 +2,61 @@
 
 #include "em/macros/meta/common.h"
 
-// `EM_IF_NONEMPTY(cond)(a)(b)` expands to `a` if `cond` isn't empty, or to `b` if it is empty. All parts can contain commas.
-#define EM_IF_NONEMPTY(...) EM_CAT(DETAIL_EM_IF_NONEMPTY_, __VA_OPT__(1))
-#define DETAIL_EM_IF_NONEMPTY_(...) DETAIL_EM_IF_ELSE_1
-#define DETAIL_EM_IF_NONEMPTY_1(...) __VA_ARGS__ DETAIL_EM_IF_ELSE_
+// Various conditional macros.
+
+
+// --- Emptiness:
+
+// Usage: `EM_IF_NONEMPTY(cond)(a)(b)`.
+// Returns `a` if `cond` isn't empty, or to `b` if it is empty. All parts can contain commas.
+#define EM_IF_NONEMPTY(...) EM_CAT(DETAIL_EM_IF_THEN_, __VA_OPT__(1))
+#define DETAIL_EM_IF_THEN_(...) DETAIL_EM_IF_ELSE_1
+#define DETAIL_EM_IF_THEN_1(...) __VA_ARGS__ DETAIL_EM_IF_ELSE_
 #define DETAIL_EM_IF_ELSE_(...)
 #define DETAIL_EM_IF_ELSE_1(...) __VA_ARGS__
 
-// If `...` isn't empty, returns it unchanged. Otherwise returns `fallback`.
-// `fallback` can contain delayed commas.
-#define EM_FALLBACK_IF_EMPTY(fallback, ...) EM_IF_NONEMPTY(__VA_ARGS__)(__VA_ARGS__)(fallback)
+// Usage: `EM_IF_EMPTY(cond)(a)(b)`.
+// A negated version of `EM_IF_NONEMPTY`, for convenience (including convenience of implementing other macros).
+#define EM_IF_EMPTY(...) EM_CAT(DETAIL_EM_IF_NOT_THEN_, __VA_OPT__(1))
+#define DETAIL_EM_IF_NOT_THEN_(...) __VA_ARGS__ DETAIL_EM_IF_ELSE_
+#define DETAIL_EM_IF_NOT_THEN_1(...) DETAIL_EM_IF_ELSE_1
 
-// `EM_IF_COMMA(cond)(a)(b)` expands to `a` if `cond` contains at least one comma, or to `b` otherwise. `a` and `b` are allowed to contain commas.
+// Usage: `EM_FALLBACK_IF_EMPTY(a)(b)`. Same as `EM_IF_NONEMPTY(a)(a)(b)`.
+// Returns `a` if it's not empty, otherwise `b`.
+#define EM_FALLBACK_IF_EMPTY(...) EM_IF_NONEMPTY(__VA_ARGS__)(__VA_ARGS__)
+
+
+// --- Commas:
+
+// Usage: `EM_IF_COMMA(cond)(a)(b)`.
+// Returns `a` if `cond` contains at least one comma, or `b` otherwise. `a` and `b` are allowed to contain commas.
 #define EM_IF_COMMA(...) DETAIL_EM_IF_COMMA(__VA_ARGS__ x)
-#define DETAIL_EM_IF_COMMA(unused, ...) EM_CAT(DETAIL_EM_IF_NONEMPTY_, __VA_OPT__(1))
+#define DETAIL_EM_IF_COMMA(unused, ...) EM_CAT(DETAIL_EM_IF_THEN_, __VA_OPT__(1))
+
+
+// --- 0 and 1:
+
+// Usage: `EM_IF_01(cond)(a)(b)`.
+// `cond` can be either `1` or `0`, then returns `a` or `b` respectively.
+// Other values of `cond` are illegal. `a` and `b` are allowed to contain commas, but `b` isn't.
+#define EM_IF_01(cond) EM_CAT(DETAIL_EM_IF_01_, cond)
+#define DETAIL_EM_IF_01_0(...) DETAIL_EM_IF_ELSE_1
+#define DETAIL_EM_IF_01_1(...) __VA_ARGS__ DETAIL_EM_IF_ELSE_
+
+
+// --- Truthy and falsey:
+
+// Usage: `EM_IF_TRUTHY(cond)(a)(b)`.
+// `1` and empty string are truthy, `0` and other non-empty strings are falsey.
+// Returns `a` if the condition is truthy, `b` otherwise. All parts are allowed to contain commas. `cond` must start with a word, since we'll concat it.
+#define EM_IF_TRUTHY(...) EM_IF_EMPTY(EM_CAT(DETAIL_EM_IF_TRUTHY_, __VA_ARGS__)())
+#define DETAIL_EM_IF_TRUTHY_()
+#define DETAIL_EM_IF_TRUTHY_1()
+
+// Usage: `EM_TRUTHY_OR_FALLBACK(a)(b)`.
+// If `a` is `1`, `0`, or an empty string, returns that. Otherwise returns `b`.
+// This should be used as a condition for `EM_IF_TRUTHY`. This gives `a` has priority over `b`. If `a` is undefined, then `b` gets used.
+#define EM_TRUTHY_OR_FALLBACK(...) EM_IF_EMPTY(EM_CAT(DETAIL_EM_TRUTHY_OR_FALLBACK_, __VA_ARGS__)())(__VA_ARGS__)
+#define DETAIL_EM_TRUTHY_OR_FALLBACK_()
+#define DETAIL_EM_TRUTHY_OR_FALLBACK_0()
+#define DETAIL_EM_TRUTHY_OR_FALLBACK_1()
