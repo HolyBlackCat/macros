a2a2d2261ca5ef09609febd47ae81db43bd65c97
HolyBlackCat <holyblckcat@gmail.com>
2025-02-07 00:35:23 +0500

    Add a comment to `EM_IMPLIES`.

--- include/em/macros/utils/implies.h
+++ include/em/macros/utils/implies.h
@@ -15,4 +15,13 @@ namespace em::detail::Macros
     };
 }
 
+// The implication operator. Usage: `a EM_IMPLIES b`.
+//
+// Similar to `a <= b`, except converts both operands to `bool` and short-circuits (if lhs is false, the rhs is not evaluated).
+//
+// Has same precedence as `||`, which is lower than everything other than `? :` and assignment, and equal to only other `||`.
+// Can have strange precedence next to `||`, e.g. `a || b EM_IMPLIES c || d` is parsed as `((a || b) EM_IMPLIES c) || d`.
+//
+// On Clang this doesn't work in concepts and requires-clauses, there it must be wrapped in `bool(...)`.
+// Otherwise Clang complains that `atomic constraint must be of type 'bool' (found '::detail::Implies')`.
 #define EM_IMPLIES ||::em::detail::Macros::Implies{}||
