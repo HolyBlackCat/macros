e48fe4c8ddf4d807b73755ffb16bb85654403e84
HolyBlackCat <holyblckcat@gmail.com>
2025-04-05 23:45:29 +0500

    Rename the named loop macro.

--- include/em/macros/utils/named_loops.h
+++ include/em/macros/utils/named_loops.h
@@ -6,7 +6,7 @@
 /* Implements named loops, allowing you to break/continue from nested loops.
 Example:
 
-for (int i = 0; i < 3; i++) EM_LOOP_NAME(my_loop)
+for (int i = 0; i < 3; i++) EM_NAMED_LOOP(my_loop)
 {
     for (int j = 0; j < 3; j++)
     {
@@ -19,7 +19,7 @@ Define `EM_KEYWORD_MACROS` to allow `break(my_loop);` and `continue(my_loop);` i
 
 */
 
-#define EM_LOOP_NAME(name) \
+#define EM_NAMED_LOOP(name) \
     /* The variable in the conditions prevents `EM_BREAK/EM_CONTINUE` from being used outside of the loop with the matching name. */\
     if ([[maybe_unused]] constexpr bool _namedloop_InvalidBreakOrContinue = false) \
     { \
@@ -32,7 +32,7 @@ Define `EM_KEYWORD_MACROS` to allow `break(my_loop);` and `continue(my_loop);` i
 #define EM_CONTINUE(name) goto EM_CAT(_namedloop_continue_,name)
 
 #if EM_ENABLE_SHORT_MACROS(NAMED_LOOPS)
-#define LOOP_NAME EM_LOOP_NAME
+#define NAMED_LOOP EM_NAMED_LOOP
 #define BREAK EM_BREAK
 #define CONTINUE EM_CONTINUE
 #endif
