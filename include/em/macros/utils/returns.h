3f73e15a8c8290753a7bb2ab1074e8a46bd51ca6
HolyBlackCat <holyblckcat@gmail.com>
2025-04-06 11:47:19 +0500

    Add missing parentheses to `EM_REQ_RETURNS()`.

--- include/em/macros/utils/returns.h
+++ include/em/macros/utils/returns.h
@@ -12,8 +12,9 @@
 //   and need to be SFINAE-disabled for some template arguments of the enclosing class.
 // I'm not entirely sure why compilers don't choke on `noexcept` here (I tested the big three) while at the same choking when
 //   trying to `decltype()` the return type like in `EM_RETURNS()`.
+// Need the `(...)` after `return` here (but in `EM_RETURNS(...)` they don't matter).
 #define EM_REQ_RETURNS(...) \
-    noexcept(noexcept(__VA_ARGS__)) -> decltype(auto) requires requires{__VA_ARGS__;} {return __VA_ARGS__;}
+    noexcept(noexcept(__VA_ARGS__)) -> decltype(auto) requires requires{__VA_ARGS__;} {return (__VA_ARGS__);}
 
 #if EM_ENABLE_SHORT_MACROS(RETURNS)
 #define RETURNS EM_RETURNS
