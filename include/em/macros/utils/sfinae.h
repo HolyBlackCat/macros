5d277c05d21cedd235feafa9745300ef7b35a594
HolyBlackCat <holyblckcat@gmail.com>
2025-10-03 00:17:01 +0500

    Rename `EM_CHECK` to `EM_CHECK_COND` to make way for the testing framework.

--- include/em/macros/utils/sfinae.h
+++ include/em/macros/utils/sfinae.h
@@ -4,8 +4,9 @@
 #include <type_traits> // IWYU pragma: keep
 
 // Check if the expression is truthy.
-#define EM_CHECK(...) ::std::enable_if_t<__VA_ARGS__, std::nullptr_t> = nullptr
-// Check if the expression is value.
+// This isn't just `EM_CHECK(...)` to avoid conflicts with our testing framework.
+#define EM_CHECK_COND(...) ::std::enable_if_t<__VA_ARGS__, std::nullptr_t> = nullptr
+// Check if the expression is valid.
 #define EM_CHECK_EXPR(...) decltype(__VA_ARGS__, nullptr) = nullptr
 // Check if the type is valid.
 // Note `identity` instead of `identity_t`, we don't want to check that the type is default-constructible.
